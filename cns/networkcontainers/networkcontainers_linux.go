6cf70dd01ef32bf0018348a8a67aa6ecdf4ac248
Sushant Sharma <sharmasushant@users.noreply.github.com>
2018-03-23 11:44:55 -0700

    Add mocks for linux containers (#19)
    Add mocks for linux containers

--- cns/networkcontainers/networkcontainers_linux.go
+++ cns/networkcontainers/networkcontainers_linux.go
@@ -8,3 +8,15 @@ import "github.com/Azure/azure-container-networking/cns"
 func createOrUpdateInterface(createNetworkContainerRequest cns.CreateNetworkContainerRequest) error {
 	return nil
 }
+
+func setWeakHostOnInterface(ipAddress string) error {
+	return nil
+}
+
+func createOrUpdateWithOperation(createNetworkContainerRequest cns.CreateNetworkContainerRequest, operation string) error {
+	return nil
+}
+
+func deleteInterface(networkContainerID string) error {
+	return nil
+}
